1bcc80e28a396b5a641482f2536f8ddbc0aeebaa
Matheus <fpmitmatheus@yahoo.com>
2022-04-11 14:04:19 -0300

    adicionando dados

--- fundamentos/bloco-04-Introdução-à-JavaScript-e-lógica-de-programação/dia-03-JavaScript-Lógica-de-Programação-e-Algoritmos/exercicio-2.js
+++ fundamentos/bloco-04-Introdução-à-JavaScript-e-lógica-de-programação/dia-03-JavaScript-Lógica-de-Programação-e-Algoritmos/exercicio-2.js
@@ -1,5 +1,7 @@
 let palavra = 'trybe'
-
-for (let letra of palavra) {
-  
-}
+let separando = palavra.split("")
+console.log(separando)
+let invertida = separando.reverse()
+console.log(invertida)
+let juntando = invertida.join("")
+console.log(juntando)
--- fundamentos/bloco-04-Introdução-à-JavaScript-e-lógica-de-programação/dia-03-JavaScript-Lógica-de-Programação-e-Algoritmos/exercicio-3.js
+++ fundamentos/bloco-04-Introdução-à-JavaScript-e-lógica-de-programação/dia-03-JavaScript-Lógica-de-Programação-e-Algoritmos/exercicio-3.js
@@ -1,7 +1,5 @@
 let array = ['java', 'javascript', 'python', 'html', 'css'];
-for (let lingua = 0; lingua < array.length; lingua += 1){
-    //console.log(array[lingua])
-    //console.log(array[lingua].length)
-    let ordem = array[lingua].length 
-    console.log(ordem)
+for (let lingua = 0; lingua < array.length; lingua += 1) {
+  let tamanho = array[lingua].length
+  console.log(tamanho)
 }
