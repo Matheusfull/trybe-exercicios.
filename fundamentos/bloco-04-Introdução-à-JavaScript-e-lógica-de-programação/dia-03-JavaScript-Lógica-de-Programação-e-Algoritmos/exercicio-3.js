f05a2e7555c566746a935880f7cfa6bb065fa0df
Matheus <fpmitmatheus@yahoo.com>
2022-04-13 10:30:56 -0300

    adicionando dados

--- fundamentos/bloco-04-Introdução-à-JavaScript-e-lógica-de-programação/dia-03-JavaScript-Lógica-de-Programação-e-Algoritmos/exercicio-3.js
+++ fundamentos/bloco-04-Introdução-à-JavaScript-e-lógica-de-programação/dia-03-JavaScript-Lógica-de-Programação-e-Algoritmos/exercicio-3.js
@@ -1,5 +1,10 @@
+let numeros = []
 let array = ['java', 'javascript', 'python', 'html', 'css'];
 for (let lingua = 0; lingua < array.length; lingua += 1) {
   let tamanho = array[lingua].length
-  console.log(tamanho)
+  numeros.push(tamanho)
 }
+let ordem = numeros.sort()
+//console.log(ordem)
+let maior = ordem[0]
+console.log(maior)
