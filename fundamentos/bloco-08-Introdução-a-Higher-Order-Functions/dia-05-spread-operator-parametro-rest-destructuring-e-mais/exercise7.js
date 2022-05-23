a39e392c9840a45d7f600fa0439518e85a9d17bc
Matheus <fpmitmatheus@yahoo.com>
2022-05-23 17:10:29 -0300

    adicionando dados

--- fundamentos/bloco-08-Introdução-a-Higher-Order-Functions/dia-05-spread-operator-parametro-rest-destructuring-e-mais/exercise7.js
+++ fundamentos/bloco-08-Introdução-a-Higher-Order-Functions/dia-05-spread-operator-parametro-rest-destructuring-e-mais/exercise7.js
@@ -18,20 +18,16 @@ const ships = [
 
 // escreva shipLength abaixo
 
-/* const shipLength = (array) => {
+ const shipLength = (array) => {
     const ships = array.map(({ name, length, measurementUnit }) => {
         // console.log(element)
         // const { name, length, measurementUnit } = ships
         return `${name} is ${length} ${measurementUnit} long`
     })
     return ships
-} */
+} 
 
-// Outra solução!
 
-const shipLength = ({ name, length, measurementUnit }) => (
-    `${name} is ${length} ${measurementUnit} long`
-)
 
 /*  console.log(shipLength(ships[0])); // 'Titanic is 269.1 meters long'
  console.log(shipLength(ships[1])); // 'Queen Mary 2 is 1132 feet long'
