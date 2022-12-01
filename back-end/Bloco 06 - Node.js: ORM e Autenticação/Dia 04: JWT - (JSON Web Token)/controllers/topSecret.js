561b7a6a8769360603ac66169d90795a1b675cb8
Matheus Felipe <fpmitmatheus@yahoo.com>
2022-12-01 12:26:24 -0500

    atualizando

--- back-end/Bloco 06 - Node.js: ORM e Autenticação/Dia 04: JWT - (JSON Web Token)/controllers/topSecret.js
+++
@@ -1,8 +0,0 @@
-module.exports = (req, res) => {
-  const { admin } = req.admin;
-
-  if (admin === true) {
-   return res.status(200).json({ secretInfo: 'Peter Parker é o Homem-Arannha' });
-  } 
-    return res.status(401).json({ message: 'Não é admin' });
-};
